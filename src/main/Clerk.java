91db02167737035247dc2fa969cb8519f185ac6d
David <david199110@hotmail.com>
2013-03-29 12:03:36 -0700

    blah

---
+++ src/main/Clerk.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+import java.sql.Connection;
+
+public class Clerk {
+	
+	private Connection con;
+	
+	public Clerk(Connection con) {
+		this.con = con;
+	}
+	
+	public void addBorrower()
+	
+	
+}
